f6e938101114b439ca021de9a02c30d6f50cfb0e
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-03 20:29:28 +0300

    fixed TELEGRAM_REGEX

--- constants/joiRegex.js
+++ constants/joiRegex.js
@@ -2,7 +2,7 @@ const NAME_REGEX = /^[a-zA-Z .'-]+$/;
 const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,64}$/;
 const PHONE_REGEX =
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{0,4}[-.\s]?\d{0,9}$/;
-const TELEGRAM_REGEX = /(?: ?^@)[a-z0-9_]{5,32}/;
+const TELEGRAM_REGEX = /(?: ?^@)[a-z0-9_]{5,32}$/;
 const YEAR_REGEX = /^\d{4,4}$/;
 const MONTH_DAY_REGEX = /^\d{2,2}$/;
 
@@ -14,24 +14,3 @@ module.exports = {
   YEAR_REGEX,
   MONTH_DAY_REGEX,
 };
-
-/**
- * PASSWORD_REGEX
- * 
-A password must contain:
-Minimum eight characters, at least one letter and one number:
-
-"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-Minimum eight characters, at least one letter, one number and one special character:
-
-"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
-Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
-
-"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
-
-"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character:
-
-"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"
- */
